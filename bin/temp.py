0acd388352cf30f77f2038cc6628e580eefb2837
Dan Cavallaro <dan.t.cavallaro@gmail.com>
2023-10-28 09:16:01 -0400

    Retry fetching temperature when DHT lib throws RuntimeError

--- bin/temp.py
+++ bin/temp.py
@@ -2,12 +2,24 @@
 
 import adafruit_dht
 from board import D25
+from retry import retry
 
 def c_to_f(temp):
   return 1.8 * temp + 32
 
-dht = adafruit_dht.DHT22(D25)
+# For unknown (to me) reasons this sometimes throws "RuntimeError: Checksum did not
+# validate. Try again.", so just automatically retry a couple times.
+@retry(RuntimeError, tries=3, delay=2)
+def get_temp(dht):
+  return dht.temperature
 
-print(c_to_f(dht.temperature))
+def print_temp_in_degrees():
+  dht = adafruit_dht.DHT22(D25)
 
-dht.exit()
+  try:
+    temp_f = c_to_f(get_temp(dht))
+    print(temp_f)
+  finally:
+    dht.exit()
+
+print_temp_in_degrees()
