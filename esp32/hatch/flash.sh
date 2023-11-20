5e32e9d6456ac5e6db52283169745cc0ad249110
Dan Cavallaro <dan.t.cavallaro@gmail.com>
2023-11-20 16:12:57 -0500

    More OTA scripts

---
+++ esp32/hatch/flash.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+../espota.py --file firmware.bin --timeout 10 --progress --debug --host_ip 0.0.0.0 --ip hatchmeifyoucan.local --auth zUpmVKpRJV5b8TpQFghg
---
+++ esp32/twinkling_stars/flash.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+../espota.py --file firmware.bin --timeout 10 --progress --debug --host_ip 0.0.0.0 --ip starlord.local --auth GBQvu7uye5qre3bOhlK5
