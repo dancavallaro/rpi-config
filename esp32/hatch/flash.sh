14e2e868e2787ed179afbf89a1a624809cb9bf80
Dan Cavallaro <dan.t.cavallaro@gmail.com>
2024-08-06 18:33:01 -0400

    Removing obsolete esp32 stuff

--- esp32/hatch/flash.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-../espota.py --file firmware.bin --timeout 10 --progress --debug --host_ip 0.0.0.0 --ip hatchmeifyoucan.local --auth zUpmVKpRJV5b8TpQFghg
--- esp32/twinkling_stars/flash.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-../espota.py --file firmware.bin --timeout 10 --progress --debug --host_ip 0.0.0.0 --ip starlord.local --auth GBQvu7uye5qre3bOhlK5
